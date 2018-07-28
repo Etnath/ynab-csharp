f351dd251bb1fc567d452623c6012261a8b72905
Kevin Giffone <kevin.giffone@gmail.com>
2018-07-28 11:36:05 +0200

    Add YnabClient file

---
+++ src/YnabClient/YnabClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace ynab_csharp
+{
+    class YnabClient : IYnabClient, IDisposable
+    {
+        private bool _disposedValue = false; // To detect redundant calls
+
+        private HttpClient _httpClient;
+
+        public YnabClient(HttpClient client = null)
+        {
+            if (client == null)
+            {
+                _httpClient = new HttpClient();
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    _httpClient.Dispose();
+                }
+
+                _disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+    }
+}
